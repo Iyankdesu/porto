05845a61a891faf9f22331651248cab12a0926a8
Muhammad Fajrian Nur Ibrahim <iyankdesu@gmail.com>
2024-08-10 14:48:11 +0800

    feat: add AnimatedCursor

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,36 @@
 import "./index.css";
+import AnimatedCursor from "react-animated-cursor";
 import Header from "./components/Header";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
 
-function App() {
-	return (
-		<div>
-			<Header />
-			<Main />
-			<Footer />
-		</div>
-	);
+export default function App() {
+  return (
+    <div className="App">
+      <AnimatedCursor
+        innerSize={16}
+        outerSize={12}
+        color="220, 53, 0"
+        outerAlpha={0.2}
+        innerScale={0.7}
+        outerScale={3}
+        clickables={[
+          "a",
+          'input[type="text"]',
+          'input[type="email"]',
+          'input[type="number"]',
+          'input[type="submit"]',
+          'input[type="image"]',
+          "label[for]",
+          "select",
+          "textarea",
+          "button",
+          ".link",
+        ]}
+      />
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  );
 }
-
-export default App;
